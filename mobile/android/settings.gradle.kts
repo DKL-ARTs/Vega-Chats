9e42dd704b239ab4cbddd4ee76ac9ec9e0d813f0
root <root@localhost.localdomain>
2026-06-19 18:03:55 +0000

    Fix: add Flutter gradle plugin to settings

--- mobile/android/settings.gradle.kts
+++ mobile/android/settings.gradle.kts
@@ -6,6 +6,12 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("com.android.application") version "7.4.2"
+    id("org.jetbrains.kotlin.android") version "1.9.22"
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0"
+}
+
 include(":app")
 
 rootProject.name = "vega_chat"
