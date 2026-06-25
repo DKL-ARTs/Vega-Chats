1167b7af9a6bb143afc180c33ce99a465027859e
Hermes <hermes@nousresearch.com>
2026-06-25 16:50:21 +0000

    Revert to http.Request with cleaned key

--- mobile/lib/core/api_client.dart
+++ mobile/lib/core/api_client.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:io';
 import 'package:http/http.dart' as http;
 
 class ApiClient {
@@ -16,42 +15,28 @@ class ApiClient {
     final body = <String, dynamic>{'messages': messages, 'model': model};
     if (files != null && files.isNotEmpty) body['files'] = files;
     final cleaned = apiKey.replaceAll(RegExp(r'[^a-zA-Z0-9\-_.]'), '');
-    final url = Uri.parse(baseUrl + '/api/chat/stream');
-    final bodyStr = jsonEncode(body);
-    final bodyBytes = utf8.encode(bodyStr);
-    final isHttps = url.scheme == 'https';
-    final port = url.port != 0 ? url.port : (isHttps ? 443 : 80);
-    final socket = isHttps
-        ? await SecureSocket.connect(url.host, port)
-        : await Socket.connect(url.host, port);
-    final sb = StringBuffer();
-    sb.writeln('POST ' + url.path + ' HTTP/1.1');
-    sb.writeln('Host: ' + url.host + ':' + port.toString());
-    sb.writeln('Content-Type: application/json');
-    sb.writeln('Content-Length: ' + bodyBytes.length.toString());
+    final uri = Uri.parse('$baseUrl/api/chat/stream');
+    final req = http.Request('POST', uri);
+    req.headers['Content-Type'] = 'application/json';
     if (cleaned.isNotEmpty) {
-      sb.writeln('Bearer ' + cleaned);
+      req.headers['Authorization'] = 'Bearer $cleaned';
     }
-    sb.writeln();
-    socket.write(sb.toString());
-    socket.add(bodyBytes);
-    await socket.flush();
-    print("[SOCKET] Request sent, waiting for response...");
-    return http.StreamedResponse(socket, 200);
+    req.body = jsonEncode(body);
+    return req.send();
   }
 
   Future<Map<String, dynamic>> readFile(String path) async {
-    final resp = await http.post(Uri.parse(baseUrl + '/api/files/read'), body: jsonEncode({'path': path}));
+    final resp = await http.post(Uri.parse('$baseUrl/api/files/read'), body: jsonEncode({'path': path}));
     return jsonDecode(resp.body);
   }
 
   Future<Map<String, dynamic>> writeFile(String path, String content) async {
-    final resp = await http.post(Uri.parse(baseUrl + '/api/files/write'), body: jsonEncode({'path': path, 'content': content}));
+    final resp = await http.post(Uri.parse('$baseUrl/api/files/write'), body: jsonEncode({'path': path, 'content': content}));
     return jsonDecode(resp.body);
   }
 
   Future<Map<String, dynamic>> listFiles(String path) async {
-    final resp = await http.post(Uri.parse(baseUrl + '/api/files/list'), body: jsonEncode({'path': path}));
+    final resp = await http.post(Uri.parse('$baseUrl/api/files/list'), body: jsonEncode({'path': path}));
     return jsonDecode(resp.body);
   }
 }
