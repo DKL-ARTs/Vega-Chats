d3bfb9bb0e01d11736894b2150d688ef7d08a14d
Hermes <hermes@nousresearch.com>
2026-07-17 08:38:08 +0000

    fix(ide): import path package as p in editor_screen

--- mobile/lib/presentation/ide/editor_screen.dart
+++ mobile/lib/presentation/ide/editor_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:io';
+import 'package:path/path.dart' as p;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
